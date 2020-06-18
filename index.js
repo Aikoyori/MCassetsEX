67b87a1e6e5f1eda99e414e8537f0961eb9d3e1c
Aikoyori <gurockherobrine123@gmail.com>
2020-06-18 10:12:28 +0700

    Changed readme a bit and removed some useless code

--- index.js
+++ index.js
@@ -3,28 +3,22 @@ const makeDir = require('make-dir');
 const cpy = require('cpy');
 const prompt = require('prompt');
 
-const properties = [
-    {
-        name: 'version',
-        validator: /^[\d\.-\w]+$/,
-        warning: 'Username must be only letters, spaces, or dashes'
-    }
-];
 var props = {
     properties: {
         version: {
         pattern: /(.*-)?.+\..+/,
-        message: 'Use actual version from the list lmao',
+        message: 'Please don\'t make up versions that you haven\'t even downloaded.',
         required: true
       },
       mode: {
-        pattern: /[0123]/,
+        pattern: /[012345]/,
         required: true,
-        message: 'What is that mode',
+        message: 'Invalid mode. Try again.',
 
       }
     }
   };
+
 // StackOverflow Magic
 function makeid(length) {
     var result           = '';
@@ -36,16 +30,18 @@ function makeid(length) {
     return result;
  }
  
- console.log("WELCOME TO THE MOST EPIC PROGRAM BY THE AIKOYORI \N\N ASSETS LISTING FROM YOUR INDEXES FOLDER : ");
+console.log("WELCOME TO THE MOST EPIC PROGRAM BY THE AIKOYORI \N\N ASSETS LISTING FROM YOUR INDEXES FOLDER : ");
 
-const assetsDir = process.env.APPDATA + "/.minecraft/assets/"
-const objDir = assetsDir + "objects/"
+
+ 
+const assetsDir = process.env.APPDATA + "/.minecraft/assets/" // TODO : MAKE THIS WORK ON OTHER OSes
+const objDir = assetsDir + "objects/"  
 const indexDir = assetsDir + "indexes/"
 fs.readdir(indexDir, (err, files) => {
     files.forEach((fileRaw, index) => {
         console.log("- " + fileRaw.substr(0,fileRaw.length-5))
     })
-    console.log('\nType the version you want assets from\n e.g. 1.16, 1.8\n\nMode 0 - name with full path seperated by period (for people who want to make music like me) \nMode 1 - Original name from the index json file (for resource pack makers)\nMode 2 - Use hashes for these file names (for people who want to analyze the relationship)\nMode 3 - Randomly generate file name + extension\nMode 4 - Fuck you why would you want to use this mode (random file name no extension)\n')
+    console.log('\nType the version you want assets from\n e.g. 1.16, 1.8\n\nMode 0 - Proper file name with full path seperated by period (for people who want to make music like me so you can distinguish hurt sounds and stuff) \nMode 1 - Proper file name from the index json file (for resource pack makers)\nMode 2 - Use hashes for these file names + file extensions (for people who want to analyze the relationship)\nMode 3 - Same as 2 but no file extension (idk why)\nMode 4 - Randomly generate file name + extension\nMode 5 - Fuck you why would you want to use this mode (random file name no extension)\n')
     prompt.start();
     
     prompt.get(props, function (err, result) {
@@ -74,12 +70,21 @@ fs.readdir(indexDir, (err, files) => {
                     fileName=(key).split('/')[(key).split('/').length-1]
 
                 }
-                if(filenameMode == 2)
+                if (filenameMode == 2) {
+                    fileName = value+"."+(key).split('/')[(key).split('/').length-1].split(".")[(key).split('/')[(key).split('/').length-1].split(".").length-1]
+                
+
+                }
+                if(filenameMode == 3)
+                {
+                    fileName=value  
+                }
+                if(filenameMode == 4)
                 {
                     
                     fileName = makeid(32)+"."+(key).split('/')[(key).split('/').length-1].split(".")[(key).split('/')[(key).split('/').length-1].split(".").length-1]
                 }
-                if(filenameMode == 3)
+                if(filenameMode == 5)
                 {
                     
                     fileName = makeid(32)
--- readme.md
+++ readme.md
@@ -1,31 +1,39 @@
-# AIKOYORI's MINECRAFT ASSETS EXTRACTOR
+# Aikoyori's minecraft assets extractor
 
-TOTALLY WORKS 100% OF THE TIME TRUST ME
+Totally works 100% of the time, trust me.
 
+## Why?
 
-## HOW TO RUN™
+Because assets extraction from assets/objects folder is a pain in the ass thing to do by hand. plus I want to try out
+writing in node.js so I can be familiar with it.
 
-1. GET NODE.JS
+![Minecraft 'objects' Folder](https://my-music.is-bad.com/2J4EKvF.png "Screenshots taken by Aikoyori")
 
-2. DOWNLOAD THIS AS ZIP AND EXTRACT THIS SOMEWHERE OR CLONE THIS USING GIT COMMAND
+![Inside one of the 'objects' Folders](https://my-music.is-bad.com/7D2Y8PF.png "Screenshots taken by Aikoyori")
 
-3. OPEN TERMINAL / COMMAND PROMPT / WHATEVER YOU PUT THESE FILES IN THEN RUN
+## How to run™
+
+1. Get node.js from [https://nodejs.org/](https://nodejs.org/).
+
+2. Download this as zip and extract this somewhere or clone this using git command.
+
+3. Open terminal / command prompt / whatever you put these files in then run.
 
 ```
 npm install
 ```
-THEN WAIT THEN
+Then wait then
 
 ```
-node index.js
+node index.Js
 ```
 
-THEN IT WILL PROMPT STUFF YOU JUST TYPE STUFF IT ASKS
+Then it will prompt stuff you just type stuff it asked.
 
-## BUGS
+## bugs
 
-PLEASE REPORT THANKS
+Please report the bugs thanks.
 
-## LICENSE
+## license
 
-INCLUDED!
+Included! I use GNU GPL v3 for no reason I just can't choose.
